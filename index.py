87893470584f71a9cbf2a18dd675d0cbb78773d6
Felix <felix@treutwein/de>
2023-12-02 01:36:03 +0100

    first draft

--- index.py
+++
@@ -1,6 +0,0 @@
-<div id="interactive-map"></div>
-<script>
-  const mapContainer = document.getElementById('interactive-map');
-  const mapComponent = document.createElement('interactive-map');
-  mapContainer.appendChild(mapComponent);
-</script>
